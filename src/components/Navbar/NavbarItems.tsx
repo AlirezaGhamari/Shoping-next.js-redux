1d4809376f4c28dfdb8aba09aef2b72f9a2485e7
Alireza <158749430+AlirezaGhamari@users.noreply.github.com>
2024-07-14 18:03:47 +0330

    pointer

--- src/components/Navbar/NavbarItems.tsx
+++ src/components/Navbar/NavbarItems.tsx
@@ -5,16 +5,16 @@ function NavbarItems() {
   return (
     <div >
       <ul className="flex gap-x-8">
-        <li className="hover:text-[#ff4955]  hover:-translate-y-1 hover:scale-110 hover: duration-300 ">
+        <li className="hover:text-[#ff4955]  hover:-translate-y-1 hover:scale-110 hover: duration-300 cursor-pointer ">
           <Link href={"/home"}>HOME</Link>
         </li>
-        <li className="hover:text-[#ff4955] hover:-translate-y-1 hover:scale-110 hover: duration-300">
+        <li className="hover:text-[#ff4955] hover:-translate-y-1 hover:scale-110 hover: duration-300 cursor-pointer">
           <Link href={"/shopAll"}>SHOP ALL</Link>
         </li>
-        <li className="hover:text-[#ff4955] hover:-translate-y-1 hover:scale-110 hover: duration-300">
+        <li className="hover:text-[#ff4955] hover:-translate-y-1 hover:scale-110 hover: duration-300 cursor-pointer">
           <Link href={"/aboutUs"}>ABOUT US</Link>
         </li>
-        <li className="hover:text-[#ff4955] hover:-translate-y-1 hover:scale-110 hover: duration-300">
+        <li className="hover:text-[#ff4955] hover:-translate-y-1 hover:scale-110 hover: duration-300 cursor-pointer">
           <Link href={"/contact"}>CONTACT</Link>
         </li>
       </ul>
